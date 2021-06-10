d1de0dafef8e27f11e417d7c69c2d3acc4ae5daf
MGarcia9 <michaeljgarcia9@gmail.com>
2021-06-09 19:54:01 -0500

    First Commit

---
+++ SalesTax/ItemsInfo.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesTax
+{
+    public class ItemsInfo
+    {
+
+        #region Constants
+
+        public const string BOOK = "book";
+        public const string CHOCOLATE = "chocolate";
+        public const string CHOCOLATES = "chocolates";
+        public const string IMPORT = "imported";
+        public const string Music = "music";
+        public const string PILLS = "pills";
+
+        private const double IMPORT_TAX = .05;
+        private const double SALES_TAX = .10;
+
+        #endregion
+
+        #region Constructor
+
+        public ItemsInfo(string item, double cost)
+        {
+            Amount = 1;
+            Cost = cost;
+            Item = item;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Amount { get; set; }
+
+        public double Cost { get; set; }
+
+        public string Item { get; set; }
+
+        public double TaxTotal { get; set; }
+
+        #endregion
+
+        #region Public Methods
+
+        public string GetEachCost()
+        {
+            double eachCost = Cost / Amount;
+            return " (" + Amount + " @ " + eachCost.ToString("0.00") + ")";
+        }
+
+        public bool Match(ItemsInfo sourceItemsInfo)
+        {
+            string lowerItem = Item.ToLower();
+            return lowerItem.Equals(sourceItemsInfo.Item.ToLower());
+        }
+
+        public void SetTaxes()
+        {
+            if (IsUnTaxed() && !IsImported())
+            {
+                return;
+            }
+
+            CalculateImportTax();
+            CalculateSalesTax();
+            AddTax();
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private void AddTax()
+        {
+            Cost += TaxTotal;
+        }
+
+        private void CalculateImportTax()
+        {
+            if (!IsImported())
+            {
+                return;
+            }
+
+            TaxTotal += Math.Ceiling(Cost * IMPORT_TAX * 20) / 20;
+        }
+
+        private void CalculateSalesTax()
+        {
+            if (IsUnTaxed())
+            {
+                return;
+            }
+
+            TaxTotal += Math.Ceiling(Cost * SALES_TAX * 20) / 20;
+        }
+
+        private bool IsImported()
+        {
+            string lowerItem = Item.ToLower();
+            return lowerItem.Contains(IMPORT);
+        }
+
+        private bool IsUnTaxed()
+        {
+            string lowerItem = Item.ToLower();
+            return
+                   lowerItem.Contains(BOOK) ||
+                   lowerItem.Contains(CHOCOLATE) ||
+                   lowerItem.Contains(CHOCOLATES) ||
+                   lowerItem.Contains(PILLS);
+        }
+
+        #endregion
+
+    }
+}
---
+++ SalesTax/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesTax
+{
+    public static class Program
+    {
+
+        #region Constants
+
+        public const string ENTER_ITEMS = "Enter the items you are purchasing (hit enter twice to view your total)";
+        public const string WELCOME = "Welcome to MyStore!";
+
+        #endregion
+
+        #region Main
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(WELCOME + "\n");
+            Console.WriteLine(ENTER_ITEMS + "\n");
+
+            ProcessSalesTax();
+
+            Console.WriteLine("\nHit enter to exit.");
+            Console.Read();
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private static void FindOrAddItemsInfo(ItemsInfo newItemInfo, List<ItemsInfo> currentItems)
+        {
+            ItemsInfo foundItemsInfo = currentItems.FirstOrDefault(ci => ci.Match(newItemInfo));
+            if (foundItemsInfo != null)
+            {
+                foundItemsInfo.Amount++;
+                foundItemsInfo.Cost += newItemInfo.Cost;
+                foundItemsInfo.TaxTotal += newItemInfo.TaxTotal;
+                return;
+            }
+
+            currentItems.Add(newItemInfo);
+        }
+
+        private static string GetItem(string input)
+        {
+            int endIndex = input.IndexOf(" at ");
+            return input.Substring(2, endIndex - 1);
+        }
+
+        private static double GetPrice(string input)
+        {
+            int endIndex = input.IndexOf(" at ");
+            return Convert.ToDouble(input.Substring(endIndex + 4));
+        }
+
+        private static void PrintReceipt(ItemsInfo itemsInfo)
+        {
+            if (itemsInfo.Amount > 1)
+            {
+                Console.WriteLine(itemsInfo.Item + ": " + itemsInfo.Cost + itemsInfo.GetEachCost());
+            }
+            else
+            {
+                Console.WriteLine(itemsInfo.Item + ": " + itemsInfo.Cost.ToString("0.00"));
+            }
+        }
+
+        private static void PrintTotals(double tax, double cost)
+        {
+            Console.WriteLine("Sales Tax: " + tax.ToString("0.00"));
+            Console.WriteLine("Total: " + cost.ToString("0.00"));
+        }
+
+        private static void ProcessSalesTax()
+        {
+            List<ItemsInfo> saleItems = new List<ItemsInfo>();
+            string input;
+
+            while (!string.IsNullOrEmpty(input = Console.ReadLine()))
+            {
+                ItemsInfo itemInfo = new ItemsInfo(GetItem(input), GetPrice(input));
+                itemInfo.SetTaxes();
+                FindOrAddItemsInfo(itemInfo, saleItems);
+            }
+
+            saleItems.ForEach(si => PrintReceipt(si));
+            PrintTotals(saleItems.Sum(si => si.TaxTotal), saleItems.Sum(si => si.Cost));
+        }
+
+        #endregion
+
+    }
+}
